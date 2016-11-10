1118daad180fda40af53058160f2cf1623cda28e
Alexandru Nistor <alex.nistor@assist.ro>
2016-11-10 12:05:36 +0200

    Added Final Script + Gif Demo

--- vertical-progress-bar/ViewController.swift
+++ vertical-progress-bar/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // MARK: - IBOutlets
+    @IBOutlet weak var progressBarView: ProgressBarView!
+    
+    // MARK: - Overriden Methods
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        // On iOS 10 it has some strange behaviour if you put this in viewDidLoad. I found that in view didAppear it works without problems.
+        progressBarView.initBar()
+        
+        // The level of progress can be set from any point in the program.
+        progressBarView.setProgressValue(90)
+        
+        delayAction(withTime: 2.0) { () in
+            self.progressBarView.setProgressValue(10)
+            
+            delayAction(withTime: 2.0) { () in
+                self.progressBarView.setProgressValue(70)
+                
+                delayAction(withTime: 2.0) { () in
+                    self.progressBarView.setProgressValue(100)
+                }
+            }
+        }
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+/**
+ General delay action time with completion handler.
+ - Parameter _time : Double value that represents the seconds after which the function will execute the completion handler.
+ - Parameter done : Function that receives void and returns void.
+ */
+func delayAction(withTime _time: Double, done: Void -> Void){
+    let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(_time * Double(NSEC_PER_SEC)))
+    dispatch_after(delayTime, dispatch_get_main_queue()) {
+        done()
     }
-
-
 }
 
