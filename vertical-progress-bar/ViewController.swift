d84599b1be36f255cb933163c7ce1c91151bbdfc
Alexandru Nistor <alex.nistor@assist.ro>
2016-11-10 10:51:50 +0200

    First Commit

---
+++ vertical-progress-bar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vertical-progress-bar
+//
+//  Created by Alexandru Nistor on 11/10/16.
+//  Copyright © 2016 ASSIST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
